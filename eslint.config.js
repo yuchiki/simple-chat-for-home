1033f6a10c7ec5db0ae5169282177d167c7b00b2
yuchiki <14884013+yuchiki@users.noreply.github.com>
2024-06-12 11:53:30 +0000

    config

--- eslint.config.js
+++ eslint.config.js
@@ -7,23 +7,15 @@ import stylistic from '@stylistic/eslint-plugin'
 export default ts.config(
   js.configs.recommended,
   ...ts.configs.strictTypeChecked,
+  stylistic.configs['recommended-flat'],
   {
     languageOptions: {
       parserOptions: {
         project: true,
         tsconfigRootDir: import.meta.dirname,
       }
-    }
-  },
-  {
-    plugins: {
-      '@stylistic': stylistic
     },
-    rules: {
-      '@stylistic/indent': ['error', 2],
-    }
-  },
-  {
+
     ignores: [ 'node_modules/', 'dist/', 'eslint.config.js']
-  }
+  },
 );
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 // express server
 
-import express from 'express';
+import express from 'express'
 
-const app = express();
-const port = "3000";
+const app = express()
+const port = '3000'
 
 app.get('/', (_, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
-});
+})
