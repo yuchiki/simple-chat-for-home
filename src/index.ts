1b98988ec60aeec9e42f5045cd7226e468ecf87f
yuchiki <14884013+yuchiki@users.noreply.github.com>
2024-06-13 09:44:03 +0000

    post get API

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 const port = '3000'
 
 const PATHS = {
@@ -20,26 +23,35 @@ app.get(PATHS.ROOT, (_, res) => {
   res.send('Hello World!')
 })
 
-app.get(PATHS.API_MESSAGES, (_, res) => {
-  const messages: Message[] = [
-    {
-      user: 'user1',
-      datetime: '2021-08-01T12:00:00',
-      message: 'Hello World!',
-    },
-    {
-      user: 'user2',
-      datetime: '2021-08-01T12:01:00',
-      message: 'Hi there!',
-    },
-    {
-      user: 'user1',
-      datetime: '2021-08-01T12:02:00',
-      message: 'How are you?',
-    },
-  ]
+const messages: Message[] = [
+  {
+    user: 'user1',
+    datetime: '2021-08-01T12:00:00',
+    message: 'Hello World!',
+  },
+  {
+    user: 'user2',
+    datetime: '2021-08-01T12:01:00',
+    message: 'Hi there!',
+  },
+  {
+    user: 'user1',
+    datetime: '2021-08-01T12:02:00',
+    message: 'How are you?',
+  },
+]
 
-  res.send(JSON.stringify(messages, null, '\n'))
+app.get(PATHS.API_MESSAGES, (_, res) => {
+  res.send(messages)
+})
+
+app.post(PATHS.API_MESSAGES, (req, res) => {
+  messages.push({
+    user: req.ip ?? 'unknown',
+    datetime: new Date().toISOString(),
+    message: req.body,
+  })
+  res.send('posted.')
 })
 
 app.listen(port, () => {
