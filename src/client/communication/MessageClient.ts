527b68ee81a5c7870d636689c44e988fd7dd9d8a
yuria <yuria.soft0615@gmail.com>
2024-06-23 14:19:28 +0900

    dockerデプロイするためのCIの作成 (#1)
    * test
    
    * 認証方式をOIDCに変更
    
    * repository名を変更
    
    * ecr レジストリwo
    
    * messageClientの定義を変更
    
    * fix typo
    
    * タグベースに変更

--- src/client/communication/MessageClient.ts
+++ src/client/communication/MessageClient.ts
@@ -1,6 +1,6 @@
 import { JSONClient } from './JSONClient.js'
 export type MessageClient = JSONClient<Record<string, never>, { name: string, message: string }, Record<string, never>, { ids: string[] }>
 
-const MESSAGE_API_URL = 'http://localhost:3000/api/v1/messages'
+const MESSAGE_API_URL = `${window.location.protocol}/api/v1/messages`
 
 export const messageClient = new JSONClient<Record<string, never>, { name: string, message: string }, Record<string, never>, { ids: string[] }>(MESSAGE_API_URL)
